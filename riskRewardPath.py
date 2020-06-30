f0b8a925776a852f6a04f89ac567ea601ef471a2
Russell Brown <33341699+RussellJBrown@users.noreply.github.com>
2020-06-29 17:45:18 -0700

    Update riskRewardPath.py
    Beginning to update risk path

--- riskRewardPath.py
+++ riskRewardPath.py
@@ -5,68 +5,90 @@
 #Creating a risk reward system
 #Creating an evaluation Function
 
-
-
-
-
-
-
-
 #King is worth 100
 #Queen is worth 9
 #Rook is worth 5
 #Knight is worth 3
 #Bishop is worth 3
 #Pawn is worth 1
-
-
-
 #Perform a move a find out next likely moves based off the move
 def performTree(Colour, piece, currentScore, validMoveList,board):
-    pass
+    currentListOfMoves = []
+    bestMove = []
+    i = 0
+    Pawn = 1
+    Knight = 3
+    Bishop = 3
+    Queen = 9
+    King = 100
 
+    while i!=5:
+        for listofMoves in currentListOfMoves:
+            xposition = listofMoves[0]
+            yposition = listofMoves[1]
+            position = board[xposition][yposition]
+        i+=1
 
+    bestMove.append(xposition)
+    bestMove.append(yposition)
+    bestMove.append(piece)
 
-
+    return bestMove
 
 #Simple decision AI
 #Based off just the current state of the board
-def evaluateBoardState(Colour,piece,currentScore,validMoveList,board):
-    bestX = -1
-    bestY = -1
-    currentScore = -1
-    bestMove = []
-    for moveList in validMoveList:
-        xMoveList = moveList[0]
-        yMoveList = moveList[1]
-        isPiece = board[xMoveList][yMoveList]
+def evaluateBoardState(move,Board):
 
-        if isPiece == "Pawn" and 1<currentScore:
-            currentScore=1
-            bestX = xMoveList
-            bestY = yMoveList
+        currentScore = 0
+        evalScore = 0
+        bestMove = []
+        moveX = move[0]
+        moveY = move[1]
 
-        elif isPiece == "Bishop" and 3<currentScore:
+        currentPosition = Board[moveX][moveY]
+
+        if currentPosition == "." and evalScore<currentScore:
+            currentScore = 0
+
+
+        elif currentPosition == "Pawn" and evalScore<currentScore:
+            currentScore = 1
+
+        elif currentPosition == "Knight" and evalScore<currentScore:
             currentScore = 3
-            bestX = xMoveList
-            bestY = yMoveList
 
-        elif isPiece == "Knight" and 3<currentScore:
+        elif currentPosition == "Bishop" and evalScore<currentScore:
             currentScore = 3
-            bestX = xMoveList
-            bestY = yMoveList
 
-        elif isPiece == "Queen" and 9<currentScore:
+        elif currentPosition == "Rook" and evalScore<currentScore:
+            currentScore = 5
+
+        elif currentPosition == "Queen" and evalScore<currentScore:
             currentScore = 9
-            bestX = xMoveList
-            bestY = yMoveList
 
-        elif isPiece == "King" and 100<currentScore:
+        elif currentPosition == "King" and evalScore<currentScore:
             currentScore = 100
-            bestX = xMoveList
-            bestY = yMoveList
 
-    bestMove.append(bestX)
-    bestMove.append(bestY)
-    bestMove.append(currentScore)
+
+        return bestMove
+
+
+def calculateBestMove(validPawns,validRooks,validKnights,validBishops,validQueen,validKing):
+
+    bestMove = []
+    bestMovePercentage = 0
+
+    for move in range(validPawns):
+        print(move)
+    for move in range(validRooks):
+        print(move)
+    for move in range(validKnights):
+        print(move)
+    for move in range(validBishops):
+        print(move)
+    for move in range(validQueen):
+        print(move)
+    for move in range(validKing):
+        print(move)
+
     return bestMove
