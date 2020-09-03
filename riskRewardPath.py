3acc52cfe1bc0434656d914cb395fcd67b3f6cd8
Russell Brown <33341699+RussellJBrown@users.noreply.github.com>
2020-09-03 16:11:16 -0700

    Update riskRewardPath.py
    Update to the file allows the computer to play the game, currently the computer is just making random moves I am working on creating the actual AI.

--- riskRewardPath.py
+++ riskRewardPath.py
@@ -1,6 +1,6 @@
-
-
-
+import sys
+import chess
+import random
 
 #Creating a risk reward system
 #Creating an evaluation Function
@@ -12,83 +12,76 @@
 #Bishop is worth 3
 #Pawn is worth 1
 #Perform a move a find out next likely moves based off the move
-def performTree(Colour, piece, currentScore, validMoveList,board):
-    currentListOfMoves = []
-    bestMove = []
-    i = 0
-    Pawn = 1
-    Knight = 3
-    Bishop = 3
-    Queen = 9
-    King = 100
+def calculateMoveValue(board,move,selectDiff,currentDepth):
 
-    while i!=5:
-        for listofMoves in currentListOfMoves:
-            xposition = listofMoves[0]
-            yposition = listofMoves[1]
-            position = board[xposition][yposition]
-        i+=1
+    #Indicates that a capture has occured
+    if "x" in move:
+        #Determine what piece is being picked
+        return 1
 
-    bestMove.append(xposition)
-    bestMove.append(yposition)
-    bestMove.append(piece)
+    #Indicates that check can occur
+    if "+" in move:
+        return 5
 
-    return bestMove
+    #Indicates that checkmate can occur
+    if "#" in move:
+        return 10000000
 
-#Simple decision AI
-#Based off just the current state of the board
-def evaluateBoardState(move,Board):
+    #Indicates that Castling on King Side
+    if "0-0" == move:
+        return 10
 
-        currentScore = 0
-        evalScore = 0
-        bestMove = []
-        moveX = move[0]
-        moveY = move[1]
+    #Indicates Castling on Queen Side
+    if "0-0-0" == move:
+        return 10
 
-        currentPosition = Board[moveX][moveY]
-
-        if currentPosition == "." and evalScore<currentScore:
-            currentScore = 0
+    else:
+        if currentDepth < selectDiff:
+            #Need to Update Move List Calculate Best Move for Player so it Could be Countered
+            board.push_san(move)
+            moveList = getMoveList(board)
+            for move in moveList:
+                return calculateMoveValue(board,move,selectDiff,currentDepth+1)
 
 
-        elif currentPosition == "Pawn" and evalScore<currentScore:
-            currentScore = 1
+        else:
+            #Currently returning 0 if no Captures or special move can occur.
+            return 0
 
-        elif currentPosition == "Knight" and evalScore<currentScore:
-            currentScore = 3
-
-        elif currentPosition == "Bishop" and evalScore<currentScore:
-            currentScore = 3
-
-        elif currentPosition == "Rook" and evalScore<currentScore:
-            currentScore = 5
-
-        elif currentPosition == "Queen" and evalScore<currentScore:
-            currentScore = 9
-
-        elif currentPosition == "King" and evalScore<currentScore:
-            currentScore = 100
-
-
-        return bestMove
-
-
-def calculateBestMove(validPawns,validRooks,validKnights,validBishops,validQueen,validKing):
-
-    bestMove = []
-    bestMovePercentage = 0
-
-    for move in range(validPawns):
-        print(move)
-    for move in range(validRooks):
-        print(move)
-    for move in range(validKnights):
-        print(move)
-    for move in range(validBishops):
-        print(move)
-    for move in range(validQueen):
-        print(move)
-    for move in range(validKing):
-        print(move)
+#Board is the Current Boards State
+#allComputerMoves is the list of all valid moves in current state
+#selectDiff int used to perform diff depth
+def performTree(board,allComputerMoves,selectDiff):
+    print("Perform Tree")
+    futureBoard = board
+    bestMove = allComputerMoves[0]
+    highestValue = -10000000
+    for move in allComputerMoves:
+        #futureBoard.push_san(move)
+        currentDepth = 0
+        value = calculateMoveValue(futureBoard,move,selectDiff,currentDepth)
+        if value > highestValue:
+            highestValue = value
+            bestMove = move
 
     return bestMove
+
+
+def checkMovesRating(board,selectDiff):
+    print("Make Computer Move")
+    allComputerMoves = getMoveList(board)
+    #return performTree(board,allComputerMoves,selectDiff)
+    return random.choice(allComputerMoves)
+
+
+def getMoveList(board):
+    moves = board.legal_moves
+    moves = str(moves)
+    split_string = moves.split("(")
+    split_string2 = split_string[1].split(")")
+    allComputerMoves = split_string2[0].split(", ")
+    return allComputerMoves
+
+
+def startingOpening(board):
+    print("Computer Does Starting Opening")
